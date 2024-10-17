cb385b6d19f0150da7d23a3ecdc229583876e9a7
nuvaga78 <77118425+nuvaga78@users.noreply.github.com>
2024-10-16 20:14:17 -0500

    Update apache-install.sh

--- 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/apache-install.sh
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/apache-install.sh
@@ -3,5 +3,5 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start  
 sudo systemctl enable httpd
-#echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
-echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+#echo "<h1>Welcome to Easylifetech Solution ! AWS Infra created using Terraform in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Welcome to Easylifetech Solution ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
